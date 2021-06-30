a17379d61afce7399077a07a123f65c202e2ca90
Andreas Möller <am@localheinz.com>
2021-06-30 23:47:00 +0200

    Fix: Run 'make coding-standards'

--- mod_form.php
+++ mod_form.php
@@ -26,7 +26,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
 defined('MOODLE_INTERNAL') || exit();
 
 require_once $CFG->dirroot . '/course/moodleform_mod.php';
