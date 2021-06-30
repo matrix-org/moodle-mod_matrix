614711abaac196f55c6195cacee1b3259e319d3c
Andreas Möller <am@localheinz.com>
2021-06-30 23:23:59 +0200

    Enhancement: Enable and configure header_comment fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,11 @@
 <?php
 
+$header = <<<TXT
+@package   mod_matrix
+@copyright 2020, New Vector Ltd (Trading as Element)
+@license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+TXT;
+
 $finder = PhpCsFixer\Finder::create()
     ->ignoreDotFiles(false)
     ->exclude([
@@ -14,6 +20,13 @@ $config = new PhpCsFixer\Config();
 $config
     ->setCacheFile(__DIR__ . '/.build/php-cs-fixer/.php-cs-fixer.cache')
     ->setFinder($finder)
-    ->setRules([]);
+    ->setRules([
+        'header_comment' => [
+            'comment_type' => 'PHPDoc',
+            'header' => $header,
+            'location' => 'after_declare_strict',
+            'separate' => 'both',
+        ],
+    ]);
 
 return $config;
