84b388885119cf0586d9b133f9b2a30811fbdaf7
Andreas Möller <am@localheinz.com>
2021-06-30 23:46:54 +0200

    Enhancement: Enable and configure no_extra_blank_lines fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -98,6 +98,23 @@ $config
             ],
         ],
         'no_alias_language_construct_call' => true,
+        'no_extra_blank_lines' => [
+            'tokens' => [
+                'break',
+                'case',
+                'continue',
+                'curly_brace_block',
+                'default',
+                'extra',
+                'parenthesis_brace_block',
+                'return',
+                'square_brace_block',
+                'switch',
+                'throw',
+                'use',
+                'use_trait',
+            ],
+        ],
     ]);
 
 return $config;
