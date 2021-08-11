d8e9e211c35ceca6f20515bbedd9087b69d47f4e
Andreas Möller <am@localheinz.com>
2021-08-11 17:09:18 +0200

    Enhancement: Implement simple container

---
+++ classes/container.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @package   mod_matrix
+ * @copyright 2020, New Vector Ltd (Trading as Element)
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
+ */
+
+namespace mod_matrix;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+final class container
+{
+    /**
+     * @var self
+     */
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function instance(): self
+    {
+        if (!self::$instance instanceof self) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
---
+++ test/unit/ContainerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @package   mod_matrix
+ * @copyright 2020, New Vector Ltd (Trading as Element)
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
+ */
+
+namespace mod_matrix\test\unit;
+
+use mod_matrix\container;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \mod_matrix\container
+ */
+final class ContainerTest extends Framework\TestCase
+{
+    public function testInstanceReturnsSameContainer()
+    {
+        $container = container::instance();
+
+        self::assertSame($container, container::instance());
+    }
+}
