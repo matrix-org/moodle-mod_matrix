150bf0f5a6489cdfbc82212fca58a2b6e3fefed6
Andreas Möller <am@localheinz.com>
2021-08-16 19:29:38 +0200

    Enhancement: Extract bootstrap.php

---
+++ test/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @package   mod_matrix
+ * @copyright 2020, New Vector Ltd (Trading as Element)
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
