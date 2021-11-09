9d0559cc7e6a01439790e10f783cb515e60070c3
Andreas Möller <am@localheinz.com>
2021-11-09 17:41:37 +0100

    Enhancement: Enable declare_strict_types fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,7 +15,6 @@ $header = <<<'TXT'
 TXT;
 
 $config = PhpCsFixer\Config\Factory::fromRuleSet(new PhpCsFixer\Config\RuleSet\Php73($header), [
-    'declare_strict_types' => false,
     'final_class' => false,
     'phpdoc_no_package' => false,
     'psr_autoloading' => false,
