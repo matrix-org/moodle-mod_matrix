db827dc4f982b743648fae274bb137563dfddae3
Andreas Möller <am@localheinz.com>
2021-11-22 17:16:01 +0100

    Fix: Reduce duplication

--- db/events.php
+++ db/events.php
@@ -13,90 +13,55 @@ use mod_matrix\Observer;
 
 \defined('MOODLE_INTERNAL') || exit();
 
-$observers = [
-    // Notes:
-    // * We use internal:false to listen *after* the DB transaction completes.
-    // * We listen to pretty much anything that will affect our state of the Matrix rooms.
-    // * If anything in this file changes, bump the version number.
-
-    [
-        'callback' => [
+$observers = (static function (): array {
+    $map = [
+        event\group_member_added::class => [
             Observer::class,
             'onGroupMemberChange',
         ],
-        'eventname' => event\group_member_added::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\group_member_removed::class => [
             Observer::class,
             'onGroupMemberChange',
         ],
-        'eventname' => event\group_member_removed::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\group_created::class => [
             Observer::class,
             'onGroupCreated',
         ],
-        'eventname' => event\group_created::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\role_assigned::class => [
             Observer::class,
             'onRoleChanged',
         ],
-        'eventname' => event\role_assigned::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\role_unassigned::class => [
             Observer::class,
             'onRoleChanged',
         ],
-        'eventname' => event\role_unassigned::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\role_capabilities_updated::class => [
             Observer::class,
             'onRoleChanged',
         ],
-        'eventname' => event\role_capabilities_updated::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\role_deleted::class => [
             Observer::class,
             'onRoleChanged',
         ],
-        'eventname' => event\role_deleted::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\user_enrolment_created::class => [
             Observer::class,
             'onUserEnrolmentChanged',
         ],
-        'eventname' => event\user_enrolment_created::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\user_enrolment_deleted::class => [
             Observer::class,
             'onUserEnrolmentChanged',
         ],
-        'eventname' => event\user_enrolment_deleted::class,
-        'internal' => false,
-    ],
-    [
-        'callback' => [
+        event\user_enrolment_updated::class => [
             Observer::class,
             'onUserEnrolmentChanged',
         ],
-        'eventname' => event\user_enrolment_updated::class,
-        'internal' => false,
-    ],
-];
+    ];
+
+    return \array_map(static function (string $event, array $callback): array {
+        return [
+            'callback' => $callback,
+            'eventname' => $event,
+            'internal' => false,
+        ];
+    }, \array_keys($map), \array_values($map));
+})();
