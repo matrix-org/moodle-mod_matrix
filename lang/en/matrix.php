74f3ce197b1a80de8605444e462925cfab0ef9b5
Andreas Möller <am@localheinz.com>
2021-11-26 09:43:35 +0100

    Fix: Sort

--- lang/en/matrix.php
+++ lang/en/matrix.php
@@ -10,28 +10,28 @@ declare(strict_types=1);
 
 \defined('MOODLE_INTERNAL') || exit();
 
-$string['pluginname'] = 'Matrix';
 $string['modulename'] = 'Matrix';
 $string['modulenameplural'] = 'Matrix';
 $string['pluginadministration'] = 'Matrix administration';
+$string['pluginname'] = 'Matrix';
 
 $string['activity_default_name'] = 'Matrix Chat';
 
-$string['settings_homeserver_heading'] = 'Homeserver Settings';
-$string['settings_homeserver_url_name'] = 'Homeserver URL';
-$string['settings_homeserver_url_description'] = 'The URL where the Matrix bot should connect to your Homeserver';
-$string['settings_access_token_name'] = 'Access Token';
 $string['settings_access_token_description'] = 'The access token the Matrix bot should use to authenticate with your Homeserver';
-$string['settings_element_url_name'] = 'Element Web URL';
+$string['settings_access_token_name'] = 'Access Token';
 $string['settings_element_url_description'] = 'The URL to your Element Web instance. If not supplied/empty, matrix.to URLs will be generated instead';
+$string['settings_element_url_name'] = 'Element Web URL';
+$string['settings_homeserver_heading'] = 'Homeserver Settings';
+$string['settings_homeserver_url_description'] = 'The URL where the Matrix bot should connect to your Homeserver';
+$string['settings_homeserver_url_name'] = 'Homeserver URL';
 
-$string['view_error_no_rooms'] = 'There are no rooms to show.';
-$string['view_error_no_groups'] = 'There are no groups.';
-$string['view_error_no_visible_groups'] = 'There are no visible groups.';
-$string['view_error_no_room_in_group'] = 'There is no room in this group.';
 $string['view_alert_many_rooms'] = 'You can see multiple rooms for this course - please pick the one you would like to visit';
 $string['view_button_join_room'] = 'Join room';
+$string['view_error_no_groups'] = 'There are no groups.';
+$string['view_error_no_room_in_group'] = 'There is no room in this group.';
+$string['view_error_no_rooms'] = 'There are no rooms to show.';
+$string['view_error_no_visible_groups'] = 'There are no visible groups.';
 
 $string['matrix:addinstance'] = 'Add/edit Matrix room links';
-$string['matrix:view'] = 'View Matrix room links';
 $string['matrix:staff'] = 'Treat the user as a staff user in Matrix rooms';
+$string['matrix:view'] = 'View Matrix room links';
