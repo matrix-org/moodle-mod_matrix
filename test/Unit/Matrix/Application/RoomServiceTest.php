76b9913299bd0d1fd3f2c7ef891f290ebdf90d84
Andreas Möller <am@localheinz.com>
2021-11-30 15:02:30 +0100

    Fix: Add tests

--- test/Unit/Matrix/Application/RoomServiceTest.php
+++ test/Unit/Matrix/Application/RoomServiceTest.php
@@ -20,8 +20,12 @@ use PHPUnit\Framework;
  * @covers \mod_matrix\Matrix\Application\RoomService
  *
  * @uses \mod_matrix\Matrix\Application\Configuration
+ * @uses \mod_matrix\Matrix\Domain\PowerLevel
  * @uses \mod_matrix\Matrix\Domain\RoomId
- * @uses \mod_matrix\Moodle\Domain\CourseId
+ * @uses \mod_matrix\Matrix\Domain\RoomName
+ * @uses \mod_matrix\Matrix\Domain\RoomTopic
+ * @uses \mod_matrix\Matrix\Domain\UserId
+ * @uses \mod_matrix\Matrix\Domain\UserIdCollection
  */
 final class RoomServiceTest extends Framework\TestCase
 {
@@ -95,5 +99,187 @@ final class RoomServiceTest extends Framework\TestCase
         );
 
         self::assertSame($expected, $url);
+    }
+
+    public function testRemoveRoomRemovesUsersInRoomWhenRoomHasOnlyBotUser(): void
+    {
+        $faker = self::faker();
+
+        $roomId = Matrix\Domain\RoomId::fromString($faker->sha1());
+
+        $userIdOfBot = Matrix\Domain\UserId::fromString($faker->sha1());
+
+        $api = $this->createMock(Matrix\Application\Api::class);
+
+        $api
+            ->expects(self::once())
+            ->method('listUsers')
+            ->willReturn(Matrix\Domain\UserIdCollection::fromUserIds());
+
+        $api
+            ->expects(self::once())
+            ->method('whoAmI')
+            ->willReturn($userIdOfBot);
+
+        $api
+            ->expects(self::once())
+            ->method('kickUser')
+            ->with(
+                self::identicalTo($roomId),
+                self::identicalTo($userIdOfBot),
+            );
+
+        $roomService = new Matrix\Application\RoomService(
+            $api,
+            Matrix\Application\Configuration::default(),
+        );
+
+        $roomService->removeRoom($roomId);
+    }
+
+    public function testCreateRoomCreatesRoom(): void
+    {
+        $faker = self::faker();
+
+        $name = Matrix\Domain\RoomName::fromString($faker->word());
+        $topic = Matrix\Domain\RoomTopic::fromString($faker->sentence());
+        $creationContent = [
+            'foo' => $faker->words(),
+            'bar' => $faker->sentence(),
+        ];
+
+        $userIdOfBot = Matrix\Domain\UserId::fromString($faker->sha1());
+
+        $roomId = Matrix\Domain\RoomId::fromString($faker->sha1());
+
+        $api = $this->createMock(Matrix\Application\Api::class);
+
+        $api
+            ->expects(self::once())
+            ->method('whoAmI')
+            ->willReturn($userIdOfBot);
+
+        $api
+            ->expects(self::once())
+            ->method('createRoom')
+            ->with(self::equalTo([
+                'creation_content' => $creationContent,
+                'initial_state' => [
+                    [
+                        'content' => [
+                            'guest_access' => 'forbidden',
+                        ],
+                        'state_key' => '',
+                        'type' => 'm.room.guest_access',
+                    ],
+                ],
+                'name' => $name->toString(),
+                'power_level_content_override' => [
+                    'ban' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                    'invite' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                    'kick' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                    'events' => [
+                        'm.room.name' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                        'm.room.power_levels' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                        'm.room.history_visibility' => Matrix\Domain\PowerLevel::staff()->toInt(),
+                        'm.room.canonical_alias' => Matrix\Domain\PowerLevel::staff()->toInt(),
+                        'm.room.avatar' => Matrix\Domain\PowerLevel::staff()->toInt(),
+                        'm.room.tombstone' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                        'm.room.server_acl' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                        'm.room.encryption' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                        'm.room.join_rules' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                        'm.room.guest_access' => Matrix\Domain\PowerLevel::bot()->toInt(),
+                    ],
+                    'events_default' => 0,
+                    'state_default' => Matrix\Domain\PowerLevel::staff()->toInt(),
+                    'redact' => Matrix\Domain\PowerLevel::redactor()->toInt(),
+                    'users' => [
+                        $userIdOfBot->toString() => Matrix\Domain\PowerLevel::bot()->toInt(),
+                    ],
+                ],
+                'preset' => 'private_chat',
+                'topic' => $topic->toString(),
+            ]))
+            ->willReturn($roomId);
+
+        $roomService = new Matrix\Application\RoomService(
+            $api,
+            Matrix\Application\Configuration::default(),
+        );
+
+        $actualRoomId = $roomService->createRoom(
+            $name,
+            $topic,
+            $creationContent,
+        );
+
+        self::assertSame($roomId, $actualRoomId);
+    }
+
+    public function testRemoveRoomRemovesUsersInRoomWhenRoomHasBotUserAndOtherUsers(): void
+    {
+        $faker = self::faker();
+
+        $roomId = Matrix\Domain\RoomId::fromString($faker->sha1());
+
+        $userIdOfBot = Matrix\Domain\UserId::fromString($faker->sha1());
+
+        $userIdOne = Matrix\Domain\UserId::fromString($faker->sha1());
+        $userIdTwo = Matrix\Domain\UserId::fromString($faker->sha1());
+        $userIdThree = Matrix\Domain\UserId::fromString($faker->sha1());
+        $userIdFour = Matrix\Domain\UserId::fromString($faker->sha1());
+
+        $userIds = [
+            $userIdOne,
+            $userIdTwo,
+            $userIdOfBot,
+            $userIdThree,
+            $userIdFour,
+        ];
+
+        $api = $this->createMock(Matrix\Application\Api::class);
+
+        $api
+            ->expects(self::once())
+            ->method('listUsers')
+            ->willReturn(Matrix\Domain\UserIdCollection::fromUserIds(...$userIds));
+
+        $api
+            ->expects(self::once())
+            ->method('whoAmI')
+            ->willReturn($userIdOfBot);
+
+        $api
+            ->expects(self::exactly(\count($userIds)))
+            ->method('kickUser')
+            ->withConsecutive(
+                [
+                    self::identicalTo($roomId),
+                    self::identicalTo($userIdOne),
+                ],
+                [
+                    self::identicalTo($roomId),
+                    self::identicalTo($userIdTwo),
+                ],
+                [
+                    self::identicalTo($roomId),
+                    self::identicalTo($userIdThree),
+                ],
+                [
+                    self::identicalTo($roomId),
+                    self::identicalTo($userIdFour),
+                ],
+                [
+                    self::identicalTo($roomId),
+                    self::identicalTo($userIdOfBot),
+                ],
+            );
+
+        $roomService = new Matrix\Application\RoomService(
+            $api,
+            Matrix\Application\Configuration::default(),
+        );
+
+        $roomService->removeRoom($roomId);
     }
 }
