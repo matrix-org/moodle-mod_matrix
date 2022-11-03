ae1d6490f497073e01eb6438706927118a46c41b
axel simon <git@axelsimon.net>
2022-11-03 23:26:25 +0000

    Update header licence to SPDX ID Apache-2.0
    Also:
    - fix update license in composer.json
    - update copyright year to 2022
    
    aligning with project license change from #1.
    
    Signed-off-by: axel simon <git@axelsimon.net>

--- test/Unit/Matrix/Infrastructure/HttpClientBasedApiTest.php
+++ test/Unit/Matrix/Infrastructure/HttpClientBasedApiTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 /**
  * @package   mod_matrix
- * @copyright 2020, New Vector Ltd (Trading as Element)
- * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @copyright 2022, New Vector Ltd (Trading as Element)
+ * @license   SPDX-License-Identifier: Apache-2.0
  */
 
 namespace mod_matrix\Test\Unit\Matrix\Infrastructure;
